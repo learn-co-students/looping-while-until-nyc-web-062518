b967dd779c729c24895c854be2c9808bf6e56372
Ronald <ronaldcuello11@gmail.com>
2018-06-14 15:03:22 -0400

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,20 @@
 def using_while
 	levitation_force = 6
-	
-	#your code here
+# where the variable is starting from
+	while levitation_force < 10
+		#will put out string below so long its true
+		puts "Wingardium Leviosa"
+
+	#string
+		levitation_force += 1
+end
 end
 
+def using_until
+	levitation_force = 6
+	until levitation_force == 10
+		puts "Wingardium Leviosa"
 
+		levitation_force += 1
+end
+end
